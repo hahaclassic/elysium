2062270d12f08797f87a5935004dcc40e529de42
hahaclassic <gavrilyukvlad221@gmail.com>
2024-07-09 00:48:52 +0300

    develop: changed type of key to TEXT

--- db/migrations/20240705134350_keys_table.sql
+++ db/migrations/20240705134350_keys_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS keys (
-    key UUID NOT NULL,
+    key TEXT NOT NULL,
     folder_id UUID NOT NULL REFERENCES folders (id) ON DELETE CASCADE,
     access_lvl INTEGER NOT NULL,
     PRIMARY KEY (key),
